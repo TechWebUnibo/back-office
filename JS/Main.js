a1c0f1d7a103306fd5d9932e33719bed0b049508
Digles <gilagledis00@gmail.com>
2021-10-15 16:48:10 +0200

    Co-authored-by: Antonio Lopez <elements72@users.noreply.github.com>

--- JS/Main.js
+++ JS/Main.js
@@ -1,17 +1,35 @@
-function showPass(){
+function showPass() {
     var pass = document.getElementById("userPassword");
 
-    if(pass.type === "password")
-    {
+    if (pass.type === "password") {
         pass.type = "text";
-    } else 
-    {
+    } else {
         pass.type = "password";
     }
 }
 //TODO da completare (fare praticamente)
-function isLogged(){
+function isLogged() {
     accToken = localStorage.token;
     pubKey = localStorage.publicKey;
 
+}
+async function createCategory() {
+    let map = {}
+    //Fetch of the categories
+    try {
+        var products = await fetch('https://site202118.tw.cs.unibo.it/api/products', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        })
+        products = await products.json()
+        for (const product of products) {
+            map[product._id] = product.name
+        }
+    }
+    catch (error) {
+        console.log(error)
+    }
+    return map;
 }
